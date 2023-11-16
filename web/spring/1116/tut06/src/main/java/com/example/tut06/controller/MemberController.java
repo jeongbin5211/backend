846e5e65f0fa67f08b98cd2955849530440ce0ae
jeongbin5211 <hjn123493@gmail.com>
2023-11-16 16:48:20 +0900

    1116

---
+++ web/spring/1116/tut06/src/main/java/com/example/tut06/controller/MemberController.java
@@ -0,0 +1,59 @@
+package com.example.tut06.controller;
+
+import com.example.tut06.dto.MemberDto;
+import com.example.tut06.mappers.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @GetMapping("")
+    public String getList() {
+        return "/member/list.html";
+    }
+
+    @GetMapping("/add")
+    public String getAdd() {
+        return "/member/add.html";
+    }
+
+//    @PostMapping("/add")
+//    @ResponseBody
+//    public String setAdd(@ModelAttribute MemberDto memberDto) {
+//        memberMapper.setAdd(memberDto);
+//        return "";
+//    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public Map<String, Object> setAdd(@ModelAttribute MemberDto memberDto) {
+
+        Map<String, Object> map = new HashMap<>();
+        if( memberDto != null) {
+            memberMapper.setAdd(memberDto);
+            map.put("msg", "success");
+        }
+
+        return map;
+    }
+
+    @GetMapping("/getMembers")
+    @ResponseBody
+    public Map<String, Object> getMembers() {
+        List<MemberDto> list = memberMapper.getMembers();
+//        System.out.println(list);
+        Map<String, Object> map = new HashMap<>();
+        map.put("members", list);
+        return map;
+    }
+}
---
+++ web/spring/1116/tut06/src/main/resources/static/db/db.sql
@@ -0,0 +1,10 @@
+use koreaitdb;
+
+create table member(
+num int not null auto_increment,
+userid varchar(20) not null,
+passwd varchar(20) not null,
+primary key(num)
+);
+
+show tables;
